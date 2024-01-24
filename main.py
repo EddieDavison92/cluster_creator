fdfeac08960f1656cdbc62f003edd803ccbd04dc
EddieDavison92 <143042680+EddieDavison92@users.noreply.github.com>
2024-01-24 23:21:14 +0000

    Updated README and instructions

--- main.py
+++ main.py
@@ -1,3 +1,15 @@
+"""
+This script creates clusters of SNOMED codes based on the transitive closure table in the SNOMED database.
+The clusters are created by specifying a list of parent codes and then finding all the child codes of those parents.
+The child codes are then converted to their current code if they have been retired.
+The clusters are output to a csv file and an xlsx file.
+The csv file is then used to create a txt file for each cluster.
+
+Instructions:
+Configure the paths to the databases and the output files.
+Configure the clusters dictionary with the clusters to create.
+"""
+
 import warnings
 import os
 import datetime
